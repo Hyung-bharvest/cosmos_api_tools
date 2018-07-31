d6773bdf57c50656cc030147ea74c2a3cf39b551
b-harvest <38277329+dlguddus@users.noreply.github.com>
2018-07-31 10:22:22 +0900

    use try and except for request error handling

--- validator_monitor.py
+++ validator_monitor.py
@@ -58,62 +58,81 @@ def get_data():
     global n_peers
 
     while True:
-        # get consensus state from seednode
-        Response_ConsensusState = requests.get("https://gaia-seeds.interblock.io/consensus_state", timeout=10)
-        if str(Response_ConsensusState) == "<Response [200]>" :
-            JSON_ConsensusState = json.loads(Response_ConsensusState.text)
-            height_round_step = JSON_ConsensusState["result"]["round_state"]["height/round/step"]
-            height_round_step_split = height_round_step.split("/")
-            height = int(height_round_step_split[0])
-            round = int(height_round_step_split[1])
-            step = int(height_round_step_split[2])
 
-            if height_before < height:
-                # get validator's commit
-                Response_CommitHeight = requests.get("https://gaia-seeds.interblock.io/commit?height=" + str(height-2), timeout=10)
-                validator_data = ""
-                if str(Response_CommitHeight) == "<Response [200]>" :
-                    JSON_CommitHeight = json.loads(Response_CommitHeight.text)
-                    for item in JSON_CommitHeight["result"]["SignedHeader"]["commit"]["precommits"] :
-                        if str(item) == "null" or item == None : pass
-                        else :
-                            if item["validator_address"] == validator_address :
-                                validator_data = item
-                                validator_height = int(validator_data["height"])
-                                validator_timestamp = str(validator_data["timestamp"])
-                                break
-                else :
-                    pass
+        try:
 
-                # send telegram message when missing commits
-                if validator_data == "" :
-                    requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
-                    requestURL = requestURL + str(datetime.datetime.now()) + ':MissingCommits!!!'
-                    response = requests.get(requestURL, timeout=10)
-                else :
-                    pass
-                height_before = height
+            # get consensus state from seednode
+            Response_ConsensusState = requests.get("https://gaia-seeds.interblock.io/consensus_state", timeout=10)
+            if str(Response_ConsensusState) == "<Response [200]>" :
+                JSON_ConsensusState = json.loads(Response_ConsensusState.text)
+                height_round_step = JSON_ConsensusState["result"]["round_state"]["height/round/step"]
+                height_round_step_split = height_round_step.split("/")
+                height = int(height_round_step_split[0])
+                round = int(height_round_step_split[1])
+                step = int(height_round_step_split[2])
 
-                # get n_peers for each nodes
-                n_peers = []
-                for i in range(0,len(node_IP_port)):
-                    response = requests.get("http://" + str(node_IP_port[i]) + "/net_info", timeout=10)
-                    n_peers.append(int(json.loads(response.text)["result"]["n_peers"]))
+                if height_before < height:
+                    # get validator's commit
+                    Response_CommitHeight = requests.get("https://gaia-seeds.interblock.io/commit?height=" + str(height-2), timeout=10)
+                    validator_data = ""
+                    if str(Response_CommitHeight) == "<Response [200]>" :
+                        JSON_CommitHeight = json.loads(Response_CommitHeight.text)
+                        for item in JSON_CommitHeight["result"]["SignedHeader"]["commit"]["precommits"] :
+                            if str(item) == "null" or item == None : pass
+                            else :
+                                if item["validator_address"] == validator_address :
+                                    validator_data = item
+                                    validator_height = int(validator_data["height"])
+                                    validator_timestamp = str(validator_data["timestamp"])
+                                    break
+                    else :
+                        pass
 
-                # send message in every 1 hour
-                if count > 720:
-                    requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
-                    requestURL = requestURL + str(datetime.datetime.now()) + ':Height=' + str(height) +'/Status:OK/Peers:'
-                    for i in range(0,len(n_peers)):
-                        requestURL = requestURL + str(n_peers[i]) + '.'
-                    response = requests.get(requestURL, timeout=10)
-                    count = 0
+                    # send telegram message when missing commits
+                    if validator_data == "" :
+                        requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
+                        requestURL = requestURL + str(datetime.datetime.now()) + ':MissingCommits!!!'
+                        response = requests.get(requestURL, timeout=10)
+                    else :
+                        pass
+                    height_before = height
 
-                count = count + 1
+                    # get n_peers for each nodes
+                    n_peers = []
+                    for i in range(0,len(node_IP_port)):
+                        response = requests.get("http://" + str(node_IP_port[i]) + "/net_info", timeout=10)
+                        n_peers.append(int(json.loads(response.text)["result"]["n_peers"]))
 
-        else :
+                    # send message in every 1 hour
+                    if count > 720:
+                        requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
+                        requestURL = requestURL + str(datetime.datetime.now()) + ':Height=' + str(height) +'/Status:OK/Peers:'
+                        for i in range(0,len(n_peers)):
+                            requestURL = requestURL + str(n_peers[i]) + '.'
+                        response = requests.get(requestURL, timeout=10)
+                        count = 0
+
+                    count = count + 1
+
+            else :
+                requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
+                requestURL = requestURL + str(datetime.datetime.now()) + ':RequestError!!!'
+                response = requests.get(requestURL, timeout=10)
+
+            time.sleep(1)
+
+        except:
             requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
             requestURL = requestURL + str(datetime.datetime.now()) + ':RequestError!!!'
             response = requests.get(requestURL, timeout=10)
+            time.sleep(10)
 
-        time.sleep(1)
+
+def flask_run():
+    app.run(host='0.0.0.0', port='5000')
+
+t1 = threading.Thread(name='flask_run', target=flask_run)
+t2 = threading.Thread(name='get_data', target=get_data)
+
+t1.start()
+t2.start()
